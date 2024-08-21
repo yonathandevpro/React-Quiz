f834af4964afb350ab0f3de42385c2a90ea156e9
yonathandevpro <yonathanber@icloud.com>
2024-08-21 14:12:58 +0300

    Loading questions from a custom API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Major from "./Major";
+
+const initialState = {
+  questions: [],
+
+  // 'loading','error', 'ready','active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
