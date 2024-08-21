2b10e3d72dc7734e620b516c9bbca66fad11e2d0
yonathandevpro <yonathanber@icloud.com>
2024-08-21 13:40:02 +0300

    Building structure for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import DateCounter from "./DateCounter";
-
+import Header from "./Header";
+import Major from "./Major";
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Major>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Major>
     </div>
   );
 }
